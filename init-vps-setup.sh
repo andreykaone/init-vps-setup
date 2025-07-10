a6a91faf7c2ee6c90b2b7c40c57ea02b0d307170
andreykaone <andreyka.one.two@gmail.com>
2025-07-10 21:57:55 +0300

    add more changes

--- init-vps-setup.sh
+++ init-vps-setup.sh
@@ -5,7 +5,8 @@ set -e  # Прерывать выполнение при ошибках
 # Проверка прав root
 if [ "$EUID" -ne 0 ]; then 
   echo "Please run as root"
-  echo $EUID
+  echo "EUID: $EUID"
+  echo "USER: $USER"
   exit 1
 fi
 
