5c9fb3df8e72906cc40d80d4e20589c530169715
andreykaone <andreyka.one.two@gmail.com>
2025-07-11 00:37:36 +0300

    ch

--- init-vps-setup.sh
+++ init-vps-setup.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 set -e  # Прерывать выполнение при ошибках
 
-# Разбор аргументов
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        -e|--user)
-            USERNAME="$2"
-            ;;
-        *)
-    esac
-done
+echo $0
+echo $1
+echo $2
 
 
-echo "перед проверкой рута"
 
-
-# Проверка прав root
-if [ "$EUID" -ne 0 ]; then 
-  echo "Пожалуйста, запускайте скрипт из-под пользователя root"
-  exit 1
-fi
-
-echo "после проверки рута"
-
-
-# Использование параметров
-echo "USERNAME: $USERNAME"
-
-
-# Создание пользователя
-if id "$1" >/dev/null 2>&1; then
-    echo "пользователь " $USERNAME " уже существует"
-else
-    adduser --disabled-password --gecos "" $USERNAME
-    echo "пользователь " $USERNAME " создан"
-fi
-
-usermod -aG sudo $USERNAME
+#echo "перед проверкой рута"
+#
+#
+## Проверка прав root
+#if [ "$EUID" -ne 0 ]; then 
+#  echo "Пожалуйста, запускайте скрипт из-под пользователя root"
+#  exit 1
+#fi
+#
+#echo "после проверки рута"
+#
+#
+## Использование параметров
+#echo "USERNAME: $USERNAME"
+#
+#
+## Создание пользователя
+#if id "$1" >/dev/null 2>&1; then
+#    echo "пользователь " $USERNAME " уже существует"
+#else
+#    adduser --disabled-password --gecos "" $USERNAME
+#    echo "пользователь " $USERNAME " создан"
+#fi
+#
+#usermod -aG sudo $USERNAME
