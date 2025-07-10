c2f68455b29cdbe25ad8006ba4163dc8f414ee9e
andreykaone <andreyka.one.two@gmail.com>
2025-07-10 23:40:28 +0300

    add small changes

--- init-vps-setup.sh
+++ init-vps-setup.sh
@@ -5,9 +5,19 @@ set -e  # Прерывать выполнение при ошибках
 # Проверка прав root
 if [ "$EUID" -ne 0 ]; then 
   echo "Please run as root"
-  echo "EUID: $EUID"
-  echo "USER: $USER"
   exit 1
 fi
 
+
+# Обновление системы
 apt update && apt full-upgrade -y
+apt install -y ufw fail2ban unattended-upgrades
+
+# Создание пользователя
+if id "$1" >/dev/null 2>&1; then
+    echo "user " $USERNAME " already exists"
+else
+    adduser --disabled-password --gecos "" $USERNAME
+    usermod -aG sudo $USERNAME
+    echo "user " $USERNAME " created"
+fi
