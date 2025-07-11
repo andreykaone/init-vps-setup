37fc8c52d08ab84893e0f9cfcd38b9c5a29be103
andreykaone <andreyka.one.two@gmail.com>
2025-07-11 09:09:01 +0300

    new test changeeees

--- init-vps-setup.sh
+++ init-vps-setup.sh
@@ -1,35 +1,7 @@
 #!/bin/bash
 set -e  # Прерывать выполнение при ошибках
 
-echo "баксики параметры:"
-echo $0
-echo $1
-echo $2
+echo -n "New username: "
+read -r $USERNAME
 
-
-
-#echo "перед проверкой рута"
-#
-#
-## Проверка прав root
-#if [ "$EUID" -ne 0 ]; then 
-#  echo "Пожалуйста, запускайте скрипт из-под пользователя root"
-#  exit 1
-#fi
-#
-#echo "после проверки рута"
-#
-#
-## Использование параметров
-#echo "USERNAME: $USERNAME"
-#
-#
-## Создание пользователя
-#if id "$1" >/dev/null 2>&1; then
-#    echo "пользователь " $USERNAME " уже существует"
-#else
-#    adduser --disabled-password --gecos "" $USERNAME
-#    echo "пользователь " $USERNAME " создан"
-#fi
-#
-#usermod -aG sudo $USERNAME
+echo "creating user: $USERNAME"
